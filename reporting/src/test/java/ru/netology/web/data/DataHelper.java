fec13d7150286b98cba2e003a880967ef3caaa05
coursar <coursar@gmail.com>
2019-11-17 03:37:03 +0300

    feat(reporting) + fix(bdd)

---
+++ reporting/src/test/java/ru/netology/web/data/DataHelper.java
@@ -0,0 +1,30 @@
+package ru.netology.web.data;
+
+import lombok.Value;
+
+public class DataHelper {
+  private DataHelper() {}
+
+  @Value
+  public static class AuthInfo {
+    private String login;
+    private String password;
+  }
+
+  public static AuthInfo getAuthInfo() {
+    return new AuthInfo("vasya", "qwerty123");
+  }
+
+  public static AuthInfo getOtherAuthInfo(AuthInfo original) {
+    return new AuthInfo("petya", "123qwerty");
+  }
+
+  @Value
+  public static class VerificationCode {
+    private String code;
+  }
+
+  public static VerificationCode getVerificationCodeFor(AuthInfo authInfo) {
+    return new VerificationCode("12345");
+  }
+}
