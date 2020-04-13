532888100c5d1ece741bbb8192c4632c63d6f8e9
coursar <coursar@localhost>
2020-04-13 23:43:09 +0500

    deps: upgrade versions

--- basics/junit5-unit/src/test/java/ru/netology/unit/BonusServiceTest.java
+++ basics/junit5-unit/src/test/java/ru/netology/unit/BonusServiceTest.java
@@ -6,23 +6,12 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class BonusServiceTest {
     @Test
-    void shouldReturn10IfAmountIs2000() {
+    void shouldCalculateBonus() {
         BonusService service = new BonusService();
         int amount = 2000;
 
         int actual = service.calculateBonus(amount);
         int expected = 10;
-
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    void shouldReturnZeroIfAmountLowerThan1000() {
-        BonusService service = new BonusService();
-        int amount = 900;
-
-        int actual = service.calculateBonus(amount);
-        int expected = 0;
 
         assertEquals(expected, actual);
     }
