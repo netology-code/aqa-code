da184a6595e604688b7e790c175b0ae0a7e3d380
coursar <coursar@localhost>
2020-04-20 19:15:54 +0500

    feat(api-ci)

---
+++ api-ci/rest/src/test/java/ru/netology/rest/MobileBankApiTestV4.java
@@ -0,0 +1,24 @@
+package ru.netology.rest;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+
+class MobileBankApiTestV4 {
+    @Test
+    void shouldReturnDemoAccounts() {
+      // Given - When - Then
+      // Предусловия
+      given()
+          .baseUri("http://localhost:9999/api/v1")
+      // Выполняемые действия
+      .when()
+          .get("/demo/accounts")
+      // Проверки
+      .then()
+          .statusCode(200)
+          .body(matchesJsonSchemaInClasspath("accounts.schema.json"))
+      ;
+    }
+}
