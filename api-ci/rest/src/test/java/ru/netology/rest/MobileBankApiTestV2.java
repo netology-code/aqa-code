b15f66b4c623cdb2e9d410b00bcf6848a7ff28ee
Андрей Пахомов <andrew-pahomov@yandex.ru>
2024-05-16 09:02:53 +0300

    update code in api-ci

--- api-ci/rest/src/test/java/ru/netology/rest/MobileBankApiTestV2.java
+++ api-ci/rest/src/test/java/ru/netology/rest/MobileBankApiTestV2.java
@@ -8,19 +8,19 @@ import static io.restassured.RestAssured.given;
 class MobileBankApiTestV2 {
     @Test
     void shouldReturnDemoAccounts() {
-      // Given - When - Then
-      // Предусловия
-      given()
-          .baseUri("http://localhost:9999/api/v1")
-      // Выполняемые действия
-      .when()
-          .get("/demo/accounts")
-      // Проверки
-      .then()
-          .statusCode(200)
-          // .header("Content-Type", "application/json; charset=UTF-8")
-          // специализированные проверки - лучше
-          .contentType(ContentType.JSON)
-      ;
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("http://localhost:9999/api/v1")
+                // Выполняемые действия
+                .when()
+                .get("/demo/accounts")
+                // Проверки
+                .then()
+                .statusCode(200)
+                .header("Content-Type", "application/json; charset=UTF-8")
+                // специализированные проверки - лучше
+                .contentType(ContentType.JSON)
+        ;
     }
 }
--- api-ci/rest/src/test/java/ru/netology/rest/MobileBankApiTestV5.java
+++ api-ci/rest/src/test/java/ru/netology/rest/MobileBankApiTestV5.java
@@ -1,28 +1,26 @@
 package ru.netology.rest;
 
 import io.restassured.http.ContentType;
-import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
-import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
-import static org.hamcrest.Matchers.*;
+import static org.hamcrest.Matchers.is;
 
 class MobileBankApiTestV5 {
     @Test
     void shouldReturnDemoAccounts() {
-      // Given - When - Then
-      // Предусловия
-      given()
-          .baseUri("http://localhost:9999/api/v1")
-      // Выполняемые действия
-      .when()
-          .get("/demo/accounts")
-      // Проверки
-      .then()
-          .statusCode(200)
-          .contentType(ContentType.JSON)
-          .body("every{ it.balance >= 0 }", is(true))
-      ;
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("http://localhost:9999/api/v1")
+                // Выполняемые действия
+                .when()
+                .get("/demo/accounts")
+                // Проверки
+                .then()
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+                .body("every{ it.balance >= 0 }", is(true))
+        ;
     }
 }
