c22f67a8b9e1d425a4831c34b670492638991c97
vladVashurin <vlad.vashurin@bk.ru>
2024-12-12 16:34:28 +0300

    gitignore commit

--- basics/testng-unit/src/main/java/ru/netology/unit/Main.java
+++
@@ -1,9 +0,0 @@
-package ru.netology.unit;
-
-public class Main {
-    public static void main(String[] args) {
-        BonusService service = new BonusService();
-        int result = service.calculateBonus(1000);
-        System.out.println(result);
-    }
-}
--- docker/node/Dockerfile
+++
@@ -1,6 +0,0 @@
-FROM node:lts-alpine3.12
-WORKDIR /opt/app
-COPY . .
-RUN npm install
-CMD ["npm", "start"]
-EXPOSE 9999
